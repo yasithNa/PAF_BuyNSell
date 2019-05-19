3f2a4d3ed964c1b8acda775bc1b0f889dbc5f9cc
yasithNa <50798450+yasithNa@users.noreply.github.com>
2019-05-19 23:43:42 +0530

    Add files via upload

---
+++ PafProject1/PafProject1/src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,71 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	
+	@Id
+	private int id;
+	private String itemName;
+	private String content;
+	private double price; 
+	
+	public Product() {
+		
+		
+	}
+
+	
+
+	public Product(int id, String itemName, String content, double price) {
+		super();
+		this.id = id;
+		this.itemName = itemName;
+		this.content = content;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", itemName=" + itemName + ", content=" + content + ", price=" + price + "]";
+	}
+	
+	
+
+}
